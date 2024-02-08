945e4ef3f31506a5aaebcb0e6e8f7377bf5fd32a
Mino-rex <macminosp@icloud.com>
2024-02-08 22:34:17 +0900

    Update connection.rb

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,7 +9,12 @@ module ApplicationCable
     private
 
       def find_verified_user
-        self.current_user = env['warden'].user
+        if verified_user = User.find_by(id: cookies.encrypted[:user_id])
+          self.current_user = env['warden'].user
+        else
+          reject_unauthorized_connection
+        end
+        
       end
   end
 end
