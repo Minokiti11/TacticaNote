b2c4ff57ce83451f0c3ab8948e0ab66b3817e553
Mino-rex <macminosp@icloud.com>
2024-02-22 19:01:42 +0900

    Update videos_controller.rb

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -24,13 +24,6 @@ class VideosController < ApplicationController
     end
 
     def destroy
-      s3 = Aws::S3::Resource.new(region:'ap-northeast-1', client: Aws::S3::Client.new(http_wire_trace: true))
-      if s3.bucket(ARGV[0]).exists?
-        puts "Bucket #{ARGV[0]} exists"
-      else
-        puts "Bucket #{ARGV[0]} does not exist"
-      end
-
       video = ActiveStorage::Attachment.find(params[:id])
       @video = Video.find(params[:id])
       if video.purge && @video.destroy
