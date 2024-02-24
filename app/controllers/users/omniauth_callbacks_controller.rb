72933867be98cba1652dd4a7ebe905f9ed283496
Mino-rex <macminosp@icloud.com>
2024-02-24 14:18:40 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,29 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    skip_before_action :verify_authenticity_token, only: :google_oauth2
+  def facebook
+    callback_for(:facebook)
+  end
 
-    def google_oauth2
-            @user = User.from_omniauth(request.env["omniauth.auth"])
-        
-            if @user.persisted?
-              sign_in_and_redirect @user, event: :authentication
-              set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
-            else
-              session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
-              redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-            end
-    end
-    
-    def failure
-      redirect_to root_path, alert: "Authentication failed, please try again."
-    end
-    
-    private
-  
-    def auth
-      auth = request.env['omniauth.auth']
+  def twitter
+    callback_for(:twitter)
+  end
+
+  def google_oauth2
+    callback_for(:google)
+  end
+
+  # common callback method
+  def callback_for(provider)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url
     end
   end
+
+  def failure
+    redirect_to root_path
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
   has_many :group_users
   has_many :groups, through: :group_users
   attachment :profile_image, destroy: false
 
   has_many :messages
 
-  devise :omniauthable, omniauth_providers: %i[google_oauth2]
-  
-  validates :uid, uniqueness: { scope: :provider }
-
+  # omniauthのコールバック時に呼ばれるメソッド
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
-      user.avatar = auth.info.image
-      user.skip_confirmation!
     end
   end
 end
