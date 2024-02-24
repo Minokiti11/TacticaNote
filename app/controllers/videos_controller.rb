e2498b4b39fa99e717940326744d696da7ddd8af
Mino-rex <macminosp@icloud.com>
2024-02-24 10:02:54 +0900

    Update videos_controller.rb

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,39 +1,37 @@
-require 'aws-sdk-s3'
-
 class VideosController < ApplicationController
-    def index
-        @videos = Video.all
+  def index
+      @videos = Video.all
+  end
+  
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to @video
+    else
+      render 'new'
     end
     
-    def new
-      @video = Video.new
-    end
-  
-    def create
-      @video = Video.new(video_params)
-      if @video.save
-        redirect_to @video
-      else
-        render 'new'
-      end
-      
-    end
-  
-    def show
-      @video = Video.find(params[:id])
-    end
+  end
 
-    def destroy
-      video = ActiveStorage::Attachment.find(params[:id])
-      @video = Video.find(params[:id])
-      video.purge
-      @video.destroy
-      redirect_to videos_path
-    end
-  
-    private
-    def video_params
-      params.require(:video).permit(:title, :introduction, :video)
-    end
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  def destroy
+    video = ActiveStorage::Attachment.find(params[:id])
+    @video = Video.find(params[:id])
+    video.purge
+    @video.destroy
+    redirect_to videos_path
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:title, :introduction, :video)
+  end
 end
   
