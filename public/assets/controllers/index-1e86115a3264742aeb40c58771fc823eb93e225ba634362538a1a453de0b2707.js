f693d61933477f4d964ce0bda1514d25ec516213
Mino-rex <macminosp@icloud.com>
2024-02-26 01:19:59 +0900

    Update public/assets

--- public/assets/controllers/index-1e86115a3264742aeb40c58771fc823eb93e225ba634362538a1a453de0b2707.js
+++ public/assets/controllers/index-1e86115a3264742aeb40c58771fc823eb93e225ba634362538a1a453de0b2707.js
@@ -5,4 +5,4 @@
 import { application } from "./application"
 
 // import HelloController from "./hello_controller"
-// application.register("hello", HelloController)
+// application.register("hello", HelloController);
