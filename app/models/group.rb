02ae8251ae1d6321555848b3c3932458fa5f2b58
Mino-rex <macminosp@icloud.com>
2024-04-24 18:18:46 +0900

    video/showのレイアウトを整えた

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,7 @@ class Group < ApplicationRecord
     has_many :users, through: :group_users, dependent: :destroy
     has_many :messages, dependent: :destroy
     has_many :videos, dependent: :destroy
+    has_many :notes, dependent: :destroy
 
     validates :name, presence: true, uniqueness: true
     validates :introduction, presence: true
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,8 @@
 class Video < ApplicationRecord
     has_one_attached :video
 
+    has_many :notes, dependent: :destroy
+
     belongs_to :user, optional: true
     belongs_to :group, optional: true
 end
