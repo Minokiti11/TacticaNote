95046414d48099e088736172517bf6039c7f885e
Mino-rex <macminosp@icloud.com>
2024-05-03 20:45:19 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile # cssはsprocketsを使っているため
-bundle exec rails assets:clean
-bundle exec rails db:migrate # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
+bundle exec rake assets:precompile # cssはsprocketsを使っているため
+bundle exec rake assets:clean
+bundle exec rake db:migrate # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
