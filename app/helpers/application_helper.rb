13ff70f767134173ca79b9409ea119c3e35083ab
Mino-rex <macminosp@icloud.com>
2024-06-21 00:37:12 +0900

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,13 +18,13 @@ module ApplicationHelper
                 description: :description,
                 type: 'website',
                 url: request.original_url,
-                image: url('image/tacticanote-logo.png'),# 配置するパスやファイル名によって変更する
+                image: 'https://tactica-chat.com/images/tacticanote-logo.png',
                 local: 'ja-JP',
             },
             twitter: {
                 card: 'summary_large_image', # Twitterで表示する場合は大きいカードに変更
                 site: '@minorex14', # アプリの公式Twitterアカウントがあれば、アカウント名を記載
-                image: url('image/tacticanote-logo.png'),# 配置するパスやファイル名によって変更
+                image: 'https://tactica-chat.com/images/tacticanote-logo.png',
             }
         }
     end
