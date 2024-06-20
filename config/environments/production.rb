0701e46a985aa55a6ceba1add8e6451455c26302
Mino-rex <macminosp@icloud.com>
2024-06-21 00:48:17 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
+  config.public_file_server.enabled = true
   config.serve_static_files = true
 
   # Compress CSS using a preprocessor.
