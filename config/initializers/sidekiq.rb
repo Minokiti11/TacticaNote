27f93c57707a7ba494664b9110c657a1b643beac
Mino-rex <macminosp@icloud.com>
2024-06-23 09:17:11 +0900

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-# Sidekiq.configure_server do |config|
-#     config.redis = {url: 'redis://red-cp9tqqf109ks73aejveg:FR2RHJPqVpB9U4IKKPNaDBJkvHk4SfTv@singapore-redis.render.com:6379'}
-# end
+Sidekiq.configure_server do |config|
+    config.redis = {url: 'redis://red-cprangdumphs73c22la0:6379'}
+end
 
-# Sidekiq.configure_client do |config|
-#     config.redis = {url: 'redis://red-cp9tqqf109ks73aejveg:FR2RHJPqVpB9U4IKKPNaDBJkvHk4SfTv@singapore-redis.render.com:6379'}
-# end
+Sidekiq.configure_client do |config|
+    config.redis = {url: 'redis://red-cprangdumphs73c22la0:6379'}
+end
+
+puts 'REDIS_URL: ',ENV['REDIS_URL'] 
