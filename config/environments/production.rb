35bbf825d944a0a9426c6bc3ed1190932a1e152c
Mino-rex <macminosp@icloud.com>
2024-06-27 13:14:02 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,6 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
   # この設定により、ActiveJobがジョブをキューに入れる際にSidekiqを使用するようになる
   config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+  config.session_store :cache_store, expire_after: 7.days
   # RailsのキャッシュストアとしてRedisを使用する設定を追加
 end
