b044cfd67bb84e6f4db3e9ba4fd88666da10c07e
Mino-rex <macminosp@icloud.com>
2024-07-11 18:22:11 +0900

    APIリクエスト部分のコードを整理

--- app/jobs/get_ai_response.rb
+++ app/jobs/get_ai_response.rb
@@ -4,6 +4,9 @@ class GetAiResponse
     RESPONSES_PER_MESSAGE = 1
     MODEL_NAME = "gpt-4"
     TEMPERATURE = 0.3
+    PROMPTS = {good: "今から入力する文章は育成年代のサッカー選手が試合を振り返って、サッカーノートの「上手くいったこと」の欄に書かれている文章です。起こった現象の理由が書かれていなかったら、現象が起こった理由を書くように促してください。書かれていたら、現象が起こった理由が明確になっていることを丁寧語で褒め、追加すべき情報を提示してください。(e.g. ビルドアップのことについて言及されていたら=>自チームと相手チームのフォーメーションが何だったか聞く)。書かれていない事実を含めるのを避けること。具体例を提示することは避けてください。ビルドアップについて書かれていたら、相手のフォーメーションと自分たちのフォーメーションを追加して書くように促してください。output only answer less than 3 sentences.",
+                bad: "今から入力する文章は育成年代の選手が試合を振り返って、サッカーノートの「上手くいかなかったこと」の欄に書かれている文章です。上手くいかなかった現象の理由が書かれていなかったら、現象が起こった理由や具体的なシチュエーションを書くように促してください。書かれていたら、現象が起こった理由が明確になっていることを丁寧語で褒め、追加すべき情報を提示してください。（e.g. ビルドアップのことについて言及されていたら=>自チームと相手チームのフォーメーションが何だったか聞く）3文以内で具体的でありながら簡潔に回答し、考えられる原因や解決策は提示しないでください。チームの戦術的な原因も書くように促すこと。output only answer.",
+                next: "今から入力する文章は育成年代のサッカー選手が試合を振り返って、サッカーノートの「次に意識すること・次までに取り組むこと」の欄に書いた文章です。実現させるために必要なものや今ある課題に対しての解決策が書かれていたら、解決策が明確になっていることを褒めてください。書かれていなかったら具体的な解決策を書くように促してください。（e.g. 「崩し方を練習する」=> 「具体的にどのような崩し方を想定していますか？」）具体的な練習方法については質問しないでください。解決策や具体例は提示しないでください。3文以内で出力してください。output only answer." }
 
     def perform(prompt, type, specific)
         p "perform is called..."
@@ -11,7 +14,7 @@ class GetAiResponse
             OpenAI::Client.new.chat(
                 parameters: {
                     model: MODEL_NAME,
-                    messages: [{ role: "system", content: "今から入力する文章はサッカーノートの「上手くいったこと」の欄に書かれている文章です。起こった現象の理由が書かれていなかったら、現象が起こった理由を書くように促してください。書かれていたら、現象が起こった理由が明確になっていることを丁寧語で褒め、追加すべき情報を提示してください。(e.g. ビルドアップのことについて言及されていたら=>自チームと相手チームのフォーメーションが何だったか聞く)。書かれていない事実を含めるのを避けること。具体例を提示することは避けてください。ビルドアップについて書かれていたら、相手のフォーメーションと自分たちのフォーメーションを追加して書くように促してください。output only answer less than 3 sentences." }, { role: "user", content: prompt}],
+                    messages: [{ role: "system", content: PROMPTS[:good] }, { role: "user", content: prompt}],
                     temperature: TEMPERATURE,
                     stream: stream_proc(type),
                     max_tokens: 200,
@@ -19,54 +22,27 @@ class GetAiResponse
                 }
             )
         elsif type == "bad"
-            if !specific
-                OpenAI::Client.new.chat(
-                    parameters: {
-                        model: MODEL_NAME,
-                        messages: [{ role: "system", content: "今から入力する文章は育成年代の選手のサッカーノートの「上手くいかなかったこと」の欄に書かれている文章です。現象が起こった理由や具体的なシチュエーションを、追加して書くように促してください。3文以内で簡潔に回答し、考えられる原因や解決策は提示しないでください。チームの戦術的な原因も書くように促すこと。ビルドアップについて書かれていたら、相手のフォーメーションと自分たちのフォーメーションを追加して書くように促してください。output only answer." }, { role: "user", content: prompt}],
-                        temperature: TEMPERATURE,
-                        stream: stream_proc(type),
-                        max_tokens: 200,
-                        n: RESPONSES_PER_MESSAGE
-                    }
-                )
-            else
-                OpenAI::Client.new.chat(
-                    parameters: {
-                        model: MODEL_NAME,
-                        messages: [{ role: "system", content: "今から入力する文章は育成年代のサッカー選手のサッカーノートの「上手くいかなかったこと」の欄に書かれている文章です。現象が起こった理由が明確になっていることを褒めてください。また、追加すべき情報を提示してください。（e.g. ビルドアップのことについて言及されていたら=>自チームと相手チームのフォーメーションが何だったか聞く）（具体的でありながら簡潔であること。）output only answer less than 3 sentences." }, { role: "user", content: prompt}],
-                        temperature: TEMPERATURE,
-                        stream: stream_proc(type),
-                        max_tokens: 200,
-                        n: RESPONSES_PER_MESSAGE
-                    }
-                )
-            end
-
+            OpenAI::Client.new.chat(
+                parameters: {
+                    model: MODEL_NAME,
+                    messages: [{ role: "system", content: PROMPTS[:bad] }, { role: "user", content: prompt}],
+                    temperature: TEMPERATURE,
+                    stream: stream_proc(type),
+                    max_tokens: 200,
+                    n: RESPONSES_PER_MESSAGE
+                }
+            )
         elsif type == "next"
-            if !specific
-                OpenAI::Client.new.chat(
-                    parameters: {
-                        model: MODEL_NAME,
-                        messages: [{ role: "system", content: "今から入力する文章は育成年代のサッカー選手が、試合を振り返って「次に意識すること・次までに取り組むこと」の欄に書かれている文章です。具体的な解決策を追加して書くように促してください。（e.g. 「崩し方を練習する」=> 「具体的にどのような崩し方を想定していますか？」）具体的な練習方法については質問しないでください。解決策や具体例は提示しないでください。3文以内で出力してください。output only answer." }, { role: "user", content: prompt}],
-                        temperature: TEMPERATURE,
-                        stream: stream_proc(type),
-                        max_tokens: 200,
-                        n: RESPONSES_PER_MESSAGE
-                    }
-                )
-            else
-                OpenAI::Client.new.chat(
-                    parameters: {
-                        model: MODEL_NAME,
-                        messages: [{ role: "system", content: "今から入力する文章は育成年代のサッカー選手が、試合を振り返って「次に意識すること・次までに取り組むこと」の欄に書かれている文章です。現象が起こった理由が明確になっていることを褒めてください。output only answer less than 3 sentences." }, { role: "user", content: prompt}],
-                        temperature: TEMPERATURE,
-                        stream: stream_proc(type),
-                        max_tokens: 200,
-                        n: RESPONSES_PER_MESSAGE
-                    }
-                )
-            end
+            OpenAI::Client.new.chat(
+                parameters: {
+                    model: MODEL_NAME,
+                    messages: [{ role: "system", content: PROMPTS[:next]}, { role: "user", content: prompt}],
+                    temperature: TEMPERATURE,
+                    stream: stream_proc(type),
+                    max_tokens: 200,
+                    n: RESPONSES_PER_MESSAGE
+                }
+            )
         end
 
     end
--- app/services/chat_gpt_service.rb
+++ app/services/chat_gpt_service.rb
@@ -8,7 +8,7 @@ class ChatGptService
     def extract_what_happend_good(prompt)
         response = @openai.chat(
         parameters: {
-            model: "gpt-4", # Required. # 使用するGPT-3のエンジンを指定
+            model: "gpt-4",
             messages: [{ role: "system", content: "今から入力する文章はサッカーノートの「上手くいったこと」の欄に書かれている文章です。起こった現象の原因を除き、起こった現象を抜き出して、出力してください。output only answer." }, { role: "user", content: prompt }],
             temperature: 0.1, # 応答のランダム性を指定
             max_tokens: 100,  # 応答の長さを指定
@@ -20,7 +20,7 @@ class ChatGptService
     def check_specific_good(prompt)
         response = @openai.chat(
         parameters: {
-            model: "gpt-4", # Required. # 使用するGPT-3のエンジンを指定
+            model: "gpt-4",
             messages: [{ role: "system", content: "今から入力する文章はサッカーノートの「上手くいったこと」の欄に書かれている文章です。上手くいった現象の理由が書かれていたら「True」を、書かれていなかったら「False」を出力してください。output only answer." }, { role: "user", content: prompt }],
             temperature: 0.3, # 応答のランダム性を指定
             max_tokens: 200,  # 応答の長さを指定
@@ -32,7 +32,7 @@ class ChatGptService
     def extract_what_happend_bad(prompt)
         response = @openai.chat(
         parameters: {
-            model: "gpt-4", # Required. # 使用するGPT-3のエンジンを指定
+            model: "gpt-4",
             messages: [{ role: "system", content: "今から入力する文章はサッカーノートの「上手くいかなかったこと」の欄に書かれている文章です。原因を除き、起こった現象を簡潔に出力してください。output only answer except full stop." }, { role: "user", content: prompt }],
             temperature: 0.1, # 応答のランダム性を指定
             max_tokens: 100,  # 応答の長さを指定
