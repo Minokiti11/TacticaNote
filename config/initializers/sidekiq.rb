0a328d6b5d186ec807f628fdcc2b8b937479181a
Mino-rex <macminosp@icloud.com>
2024-07-12 18:02:54 +0900

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
     # config.redis = {url: 'redis://red-cprosnd6l47c73dqiaag:6379'}
-    config.logger.level = Logger::WARN
+    # config.logger.level = Logger::WARN
 end
 
 # Sidekiq.configure_client do |config|
