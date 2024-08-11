3545cfcd6f00d6bdd06e314fd5a61a212fa3daa4
Mino-rex <macminosp@icloud.com>
2024-08-11 23:40:00 +0900

    Limit users shown to those in group

--- app/controllers/mentions_controller.rb
+++ app/controllers/mentions_controller.rb
@@ -1,7 +1,8 @@
 class MentionsController < ApplicationController
-    def index 
-        @users = User.all
-
+    def index
+        @group = Group.find(current_user.group_users[0].group_id)
+        @users = @group.users
+        
         respond_to do |format|
             format.json
         end
