d20d9e334b9c5d7cc5c2f18d0141bebbb453dc52
Mino-rex <macminosp@icloud.com>
2024-08-11 16:18:41 +0900

    Disable chat function part

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -39,74 +39,74 @@ isOnPage = function(controller, action) {
 };
 
 window.onload = function() {
-  onPageLoad('groups#chat', function() {
-    const subscriptions = consumer.subscriptions.subscriptions;
-    const userId = $('#messages').data('user_id');
-    const userIdentifier = `"user_id":"${userId}"`;
+  // onPageLoad('groups#chat', function() {
+  //   const subscriptions = consumer.subscriptions.subscriptions;
+  //   const userId = $('#messages').data('user_id');
+  //   const userIdentifier = `"user_id":"${userId}"`;
   
-    if (subscriptions) {
-      subscriptions.map(function userUnsubscribe(subscription) {
-        if (subscription.identifier.includes(userIdentifier)) {
-          subscription.consumer.subscriptions.remove(subscription)
-        };
-      });
-    };
+  //   if (subscriptions) {
+  //     subscriptions.map(function userUnsubscribe(subscription) {
+  //       if (subscription.identifier.includes(userIdentifier)) {
+  //         subscription.consumer.subscriptions.remove(subscription)
+  //       };
+  //     });
+  //   };
   
-    let already_connected = false
-    for (let subscription of consumer.subscriptions.subscriptions) {
-      let already_connected_room_id = JSON.parse(subscription.identifier).room_id;
-      if (already_connected_room_id === room_id) {
-        already_connected = true
-        break
-      }
-    }
+  //   let already_connected = false
+  //   for (let subscription of consumer.subscriptions.subscriptions) {
+  //     let already_connected_room_id = JSON.parse(subscription.identifier).room_id;
+  //     if (already_connected_room_id === room_id) {
+  //       already_connected = true
+  //       break
+  //     }
+  //   }
   
-    if (already_connected){
-      return
-    }
+  //   if (already_connected){
+  //     return
+  //   }
   
-    // const chatChannelを追記
-    const chatChannel = consumer.subscriptions.create({
-      channel: 'RoomChannel', group: $('#messages').data('room_id'), user: userId  }, {
-      connected() {
-        // Called when the subscription is ready for use on the server
-        console.log("connected.");
-      },
+  //   // const chatChannelを追記
+  //   const chatChannel = consumer.subscriptions.create({
+  //     channel: 'RoomChannel', group: $('#messages').data('room_id'), user: userId  }, {
+  //     connected() {
+  //       // Called when the subscription is ready for use on the server
+  //       console.log("connected.");
+  //     },
   
-      disconnected() {
-        console.log("disconnected.");
-      },
+  //     disconnected() {
+  //       console.log("disconnected.");
+  //     },
   
-      // room_channel.rbでブロードキャストされたデータがreceivedに届き、アラート表示を実行。
-      // アラート表示する内容は「data([‘message’])」
-      // 「event.target.value」で取得したデータと同じ
-      received(data) {
-        console.log("recieved.");
-        // Called when there's incoming data on the websocket for this channel
-        const messages = document.getElementById('messages');
-        messages.insertAdjacentHTML('beforeend', data['message']);
-      },
+  //     // room_channel.rbでブロードキャストされたデータがreceivedに届き、アラート表示を実行。
+  //     // アラート表示する内容は「data([‘message’])」
+  //     // 「event.target.value」で取得したデータと同じ
+  //     received(data) {
+  //       console.log("recieved.");
+  //       // Called when there's incoming data on the websocket for this channel
+  //       const messages = document.getElementById('messages');
+  //       messages.insertAdjacentHTML('beforeend', data['message']);
+  //     },
   
-      // 仮引数 function(message)のmessage
-      // 実引数 event.target.value
-      // room_channel.rbのspeakアクションを動かすために、speak関数を定義
-      speak: function (message) {
-        console.log("group_id:");
-        console.log($('#messages').data('room_id'));
-        return this.perform('speak', { message: message, group_id: $('#messages').data('room_id'), user_id: $('#messages').data('user_id')});
-      }
-    });
+  //     // 仮引数 function(message)のmessage
+  //     // 実引数 event.target.value
+  //     // room_channel.rbのspeakアクションを動かすために、speak関数を定義
+  //     speak: function (message) {
+  //       console.log("group_id:");
+  //       console.log($('#messages').data('room_id'));
+  //       return this.perform('speak', { message: message, group_id: $('#messages').data('room_id'), user_id: $('#messages').data('user_id')});
+  //     }
+  //   });
   
-    // フォーム内でEnterキーが押された時の動作を記述
-    window.document.onkeydown = function (event) {
-      if (event.key !== 'Enter' || event.isComposing) {
-        return
-      }
-      chatChannel.speak(event.target.value);
-      event.target.value = '';
-      event.preventDefault();
-    };
-  });
+  //   // フォーム内でEnterキーが押された時の動作を記述
+  //   window.document.onkeydown = function (event) {
+  //     if (event.key !== 'Enter' || event.isComposing) {
+  //       return
+  //     }
+  //     chatChannel.speak(event.target.value);
+  //     event.target.value = '';
+  //     event.preventDefault();
+  //   };
+  // });
 
   onPageLoad('sessions#new', function() {
     // フォーム内でEnterキーが押された時の動作を記述
