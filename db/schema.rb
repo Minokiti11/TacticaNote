953c7c2f3aeda2f85612ce725fd471340dd596d4
Mino-rex <macminosp@icloud.com>
2024-08-11 13:20:18 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_14_043113) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_11_025534) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -91,6 +91,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_14_043113) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "video_id"
+    t.string "note_type"
+    t.text "discuss"
     t.index ["group_id"], name: "index_notes_on_group_id"
     t.index ["user_id"], name: "index_notes_on_user_id"
     t.index ["video_id"], name: "index_notes_on_video_id"
