36722007eb6e6413588b907d3dd75ef0e61d11d8
Mino-rex <macminosp@icloud.com>
2024-08-22 23:50:49 +0900

    [fix] Resolve issue preventing drop-down toggle from opening on pages outside the Groups controller
    Resolved by tracking group id in the session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configurepermitted_parameters, if: :devise_controller?
+    before_action :set_current_group
     
     def self.render_with_signed_in_user(user, *args)
         ActionController::Renderer::RACK_KEY_TRANSLATION['warden'] ||= 'warden'
@@ -12,5 +13,14 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
         devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email])
         devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
+
+    private
+
+    def set_current_group
+        if params[:group_id]
+            session[:current_group_id] = params[:group_id]
+        end
+        @id = session[:current_group_id]
     end
 end
