5458e7cc42d7bb79607e7a3196145e6ff16856ad
Mino-rex <macminosp@icloud.com>
2024-09-06 22:33:44 +0900

    [add] Enable image attachment with ActionText

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -2,5 +2,7 @@ class Practice < ApplicationRecord
     belongs_to :group, optional: true
     belongs_to :user, optional: true
 
+    has_rich_text :content
+
     validates :links, format: { with: URI::regexp(%w[http https]), message: "有効なURLを入力してください" }, allow_blank: true
 end
