3de32bf812438bfccf128b850238a511d4ec7c03
Mino-rex <macminosp@icloud.com>
2024-10-31 18:08:12 +0900

    Update index.js & public/assets

--- public/assets/controllers/index-9bab93115264676095d5171e90fc8d543ccabcccabaad689ebd086e777f77c0c.js
+++ public/assets/controllers/index-9bab93115264676095d5171e90fc8d543ccabcccabaad689ebd086e777f77c0c.js
@@ -6,4 +6,4 @@ import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+eagerLoadControllersFrom("controllers", application);
