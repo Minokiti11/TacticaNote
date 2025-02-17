8aa07dac1c67355b0d72b3ca4083286045d0deb3
Mino-rex <macminosp@icloud.com>
2025-02-17 21:25:56 +0900

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     OmniAuth.config.allowed_request_methods = [:post, :get]
+    provider :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :client_secret),
+    {
+        scope: 'email,profile',
+        prompt: 'select_account',
+        provider_ignores_state: false  # CSRFエラーを防ぐための設定
+    }
 end
