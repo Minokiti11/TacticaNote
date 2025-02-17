a281e5c8611de2b1801d316c76d6058acb4f8a3f
Mino-rex <macminosp@icloud.com>
2025-02-17 11:31:22 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "debug")
 
   # Prevent health checks from clogging up the logs.
   config.silence_healthcheck_path = "/up"
