a56f2040c7848ba71fd9a02ff269d67675d18943
Mino-rex <macminosp@icloud.com>
2025-02-17 22:33:53 +0900

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     OmniAuth.config.allowed_request_methods = [:post]
     OmniAuth.config.request_validation_phase = nil if Rails.env.development?
-    OmniAuth.config.read_timeout = 120
 end
