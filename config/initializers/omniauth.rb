43c93cde9587b5ca16d2a8765d10eafadc094f8a
Mino-rex <macminosp@icloud.com>
2025-02-18 14:42:07 +0900

    Update sessions/new

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    OmniAuth.config.allowed_request_methods = [:post]
-    OmniAuth.config.request_validation_phase = nil if Rails.env.development?
-end
