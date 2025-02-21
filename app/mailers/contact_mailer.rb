a1e34262f2c6b48d6a6d4a813633c68566488af5
Mino-rex <macminosp@icloud.com>
2025-02-21 16:37:43 +0900

    Update contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def inquiry(contact_form)
     @contact_form = contact_form
     mail(
-      from: 'TacticaChat <no-reply@tactica-note.com>',
+      from: 'TacticaNote <no-reply@tactica-note.com>',
       subject: 'お問い合わせを受け付けました',
       to: @contact_form.email
     )
@@ -11,7 +11,7 @@ class ContactMailer < ApplicationMailer
   def received(contact_form)
     @contact_form = contact_form
     mail(
-      from: 'TacticaChat <no-reply@tactica-note.com>',
+      from: 'TacticaNote <no-reply@tactica-note.com>',
       to: 'macminosp@icloud.com',
       subject: '【新規お問い合わせ】Webサイトからのお問い合わせ',
       reply_to: contact_form.email
