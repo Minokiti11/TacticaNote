02c23525aee677cd7346999c0d99ed2814d308bb
Mino-rex <macminosp@icloud.com>
2025-03-09 15:25:34 +0900

    Update importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,25 +1,22 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@8.0.3/app/javascript/turbo/index.js"
+pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.1.3/src/index.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@7.1.3-2/app/assets/javascripts/actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
-pin "jquery", to: "jquery.min.js", preload: true
-
-# Uppy関連のパッケージを追加
-pin "@uppy/core", to: "https://ga.jspm.io/npm:@uppy/core@3.8.0/dist/uppy.min.js"
-pin "@uppy/dashboard", to: "https://ga.jspm.io/npm:@uppy/dashboard@3.7.1/dist/uppy-dashboard.min.js"
-pin "@uppy/aws-s3-multipart", to: "https://ga.jspm.io/npm:@uppy/aws-s3-multipart@3.10.0/dist/uppy-aws-s3-multipart.min.js"
-
-pin "channels", to: "channels.js"
-pin "@rails/actioncable", to: "actioncable.esm.js"
-pin "popper", to: "@popperjs/core"
-pin "bootstrap", to: "bootstrap.min.js"
-pin "@rails/activestorage", to: "activestorage.esm.js"
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@8.0.3/dist/turbo.es2017-esm.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.1.3-2/app/assets/javascripts/activestorage.esm.js"
+pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js"
+pin "trix", to: "https://unpkg.com/trix@2.1.7/dist/trix.umd.min.js", preload: true
+pin "@rails/actiontext", to: "actiontext.js"
+pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
-pin_all_from "app/javascript/channels", under: "channels"
-pin_all_from "app/javascript", under: "javascript"
