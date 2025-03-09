c8f8164d350cb08c2f5ed8f21a7c13be4c07070a
Mino-rex <macminosp@icloud.com>
2025-03-09 15:44:52 +0900

    Update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,4 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join("public", "dist")
 
-Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js modal.js controllers/application.js controllers/clipboard_controller.js controllers/diff_content_controller.js)
+Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js modal.js controllers/application.js controllers/clipboard_controller.js controllers/diff_content_controller.js dist/videojs-zoom.js dist/videojs-zoom.css)
