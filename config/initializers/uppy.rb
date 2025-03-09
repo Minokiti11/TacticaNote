da5558f1edbcd06c22794da94269c79610c510c6
Mino-rex <macminosp@icloud.com>
2025-03-09 12:37:01 +0900

    Update uppy.rb

--- config/initializers/uppy.rb
+++ config/initializers/uppy.rb
@@ -1,6 +1,8 @@
 require 'uppy/s3_multipart'
 
-Uppy.configure do |config|
-  config.upload_options[:acl] = 'private'
-  config.upload_options[:cache_control] = 'max-age=31536000'
+Uppy::S3Multipart.configure do |config|
+  config.upload_options = {
+    acl: 'private',
+    cache_control: 'max-age=31536000'
+  }
 end
