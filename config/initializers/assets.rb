22f863ace00b3a470da14bbe3b60621167786f55
Mino-rex <macminosp@icloud.com>
2025-03-09 15:40:00 +0900

    Update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,6 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("public", "dist")
 
 Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js modal.js controllers/application.js controllers/clipboard_controller.js controllers/diff_content_controller.js)
