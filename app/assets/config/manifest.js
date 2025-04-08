fd3547685f05622aa8f8b3211eaed1c771fa5750
Mino-rex <macminosp@icloud.com>
2025-04-08 12:59:10 +0900

    Add builds/

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,8 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-//= link channels/consumer.js
-//= link channels/index.js
-//= link channels/room_channel.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
+//= link ../../javascript/channels
+//= link ../../javascript/controllers
