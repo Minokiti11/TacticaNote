b5f2f781e35bf1ebf33f5ec5695af4f41edeb647
Mino-rex <macminosp@icloud.com>
2025-04-17 11:47:22 +0900

    Add Nil-Check for execution requires group or group_id & Display navigation to join the group when current_user haven't joined any groups.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    
+    if current_user
+      p "current_user", current_user
+      if current_user.groups.length != 0
+        p current_user.groups
+        if !session[:current_group_id]
+          @group_id = current_user.groups[0].id
+        else
+          @group_id = session[:current_group_id]
+        end
+        @group = Group.find(@group_id)
+      else
+        @group = nil
+        @group_id = nil
+      end
+    end
   end
 
   def about
